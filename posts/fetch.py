1e846f1e15b38d806c664c8d616f49bc3d7d57fd
Nathan Conroy <nathan.conroy@protonmail.com>
2021-12-12 20:41:12 -0600

    Add active flag to posts. Do not resp w inactive.

--- posts/fetch.py
+++ posts/fetch.py
@@ -8,12 +8,18 @@ import yaml
 TITLE = 'TITLE'
 FILES_DIR = './posts/files'
 
+ID = 'id'
+ACTIVE = 'active'
+AUTHOR = 'author'
+DATE = 'date'
+CONTENT = 'content'
+
 
 def fetch():
     posts = []
-    for x in pathlib.Path(FILES_DIR).iterdir():
-        if x.is_file():
-            posts.append(_parse_file(x))
+    for f in pathlib.Path(FILES_DIR).iterdir():
+        if f.is_file():
+            _add_post(_parse_file(f), posts)
     return posts
 
 
@@ -21,6 +27,16 @@ def _parse_file(file: pathlib.Path):
     return yaml.safe_load(
         file.read_text()
     )
+
+
+def _add_post(post, posts):
+    if is_active(post):
+        posts.append(post)
+
+
+def is_active(post):
+    return post[ACTIVE]
+
 
 if __name__ == '__main__':
     print(fetch())
--- posts/tests/test_fetch.py
+++ posts/tests/test_fetch.py
@@ -2,7 +2,48 @@
 Tests the fetch module.
 """
 
+import copy
+
 import posts.fetch as ft
 
+
+TEST_POST = {
+    ft.ID: 1,
+    ft.ACTIVE: True,
+    ft.AUTHOR: "Nathan Conroy",
+    ft.DATE: '2021-09-20',
+    ft.TITLE: "Test post title.",
+    ft.CONTENT: "Test post content."
+}
+
+
+INACT_TEST_POST = {
+    ft.ID: 1,
+    ft.ACTIVE: False,
+    ft.AUTHOR: "Nathan Conroy",
+    ft.DATE: '2021-09-20',
+    ft.TITLE: "Test post title.",
+    ft.CONTENT: "Test post content."
+}
+
+
+def get_test_post(post):
+    """
+    Prepare a test post for use in a test.
+    """
+    return copy.deepcopy(post)
+
+
 def test_fetch():
+    """
+    Test fetching posts.
+    """
     assert isinstance(ft.fetch(), list)
+
+
+def test_is_active():
+    """
+    Test checking that a post is active.
+    """
+    assert ft.is_active(get_test_post(TEST_POST))
+    assert not ft.is_active(get_test_post(INACT_TEST_POST))
